55a02a894e1af6bed08c28d7569c0b5e5c04e1a0
David Halls <dahalls@gmail.com>
2020-01-15 23:05:47 +0000

    Fix reporter

--- test/load_runner.js
+++ test/load_runner.js
@@ -1,3 +1,28 @@
 /*eslint-env node */
+
+var util = require('util'),
+    os = require('os');
+
+console.log = function () // eslint-disable-line no-console
+{
+    process.stdout.write(util.format.apply(this, arguments));
+    process.stdout.write(os.EOL);
+};
+
+console.error = function () // eslint-disable-line no-console
+{
+    process.stderr.write(util.format.apply(this, arguments));
+    process.stderr.write(os.EOL);
+};
+
+console.trace = function trace() // eslint-disable-line no-console
+{
+    var err = new Error();
+    err.name = 'Trace';
+    err.message = util.format.apply(this, arguments);
+    Error.captureStackTrace(err, trace);
+    this.error(err.stack);
+};
+
 require = require('esm')(module); // eslint-disable-line no-global-assign
 module.exports = require('./runner.js').default;
