fe14327cbce531f383288f1b853624c3213dd6fe
David Halls <dahalls@gmail.com>
2020-01-16 07:13:51 +0000

    Fix lint

--- test/load_runner.js
+++ test/load_runner.js
@@ -3,20 +3,17 @@
 var util = require('util'),
     os = require('os');
 
-console.log = function () // eslint-disable-line no-console
-{
+console.log = function () { // eslint-disable-line no-console
     process.stdout.write(util.format.apply(this, arguments));
     process.stdout.write(os.EOL);
 };
 
-console.error = function () // eslint-disable-line no-console
-{
+console.error = function () { // eslint-disable-line no-console
     process.stderr.write(util.format.apply(this, arguments));
     process.stderr.write(os.EOL);
 };
 
-console.trace = function trace() // eslint-disable-line no-console
-{
+console.trace = function trace() { // eslint-disable-line no-console
     var err = new Error();
     err.name = 'Trace';
     err.message = util.format.apply(this, arguments);
